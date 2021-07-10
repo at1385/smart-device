c4502be66feb46d2fff9c8278c53bc8a5f68e2a5
Alexander Tikhonov <at1385@yandex.ru>
2021-07-11 04:32:58 +0500

    gulpfile.js|Правит task конвертации "webp"

--- gulpfile.js
+++ gulpfile.js
@@ -60,9 +60,9 @@ gulp.task('img', function () {
 });
 
 gulp.task('webp', function () {
-  return gulp.src('source/img/**/*.{png,jpg}')
+  return gulp.src('source/img/content-*.{png,jpg}')
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest('source/img'));
+    .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('sprite', function () {
