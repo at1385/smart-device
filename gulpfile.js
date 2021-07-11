baa0092e623dbd4fb836fb58bf510466f8957932
Alexander Tikhonov <at1385@yandex.ru>
2021-07-11 05:08:15 +0500

    gulpfile.js|Создаёт отслеживание и автообновление js/ в dev режиме

--- gulpfile.js
+++ gulpfile.js
@@ -44,6 +44,7 @@ gulp.task('server', function () {
   gulp.watch('source/img/**/*.{png,jpg,svg}', gulp.series('cleanimg', 'copyimg', 'refresh'));
   gulp.watch('source/img/inline-*.svg', gulp.series('sprite', 'html', 'refresh'));
   gulp.watch('source/*.html', gulp.series('html', 'refresh'));
+  gulp.watch('source/js/*.js', gulp.series('js', 'refresh'));
 });
 
 gulp.task('refresh', function (done) {
