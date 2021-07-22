7438f2c8edfedeaa480c909a7f02bdaf3f927ec6
Alexander Tikhonov <at1385@yandex.ru>
2021-07-23 01:09:08 +0500

    Реализует попадание полей модального окна в localStorage

--- source/js/modal.js
+++ source/js/modal.js
@@ -5,7 +5,23 @@
   var askModalOpenButton = document.querySelector('.page-header__modal-open');
   var askModal = document.querySelector('.ask-form--modal');
   var askModalCloseButton = askModal.querySelector('.ask-form__modal-close');
+  var askModalCustomerName = askModal.querySelector('#ask-modal-customer-name');
+  var askModalCustomerPhone = askModal.querySelector('#ask-modal-customer-tel');
+  var askModalCustomerQuestion = askModal.querySelector('#ask-modal-customer-question');
   var overlay = document.querySelector('.overlay');
+
+  var isStorageSupport = true;
+  var nameStorage = '';
+  var phoneStorage = '';
+  var questionStorage = '';
+
+  try {
+    nameStorage = localStorage.getItem('askModalCustomerName');
+    phoneStorage = localStorage.getItem('askModalCustomerPhone');
+    questionStorage = localStorage.getItem('askModalCustomerQuestion');
+  } catch (err) {
+    isStorageSupport = false;
+  }
 
   function isEscKeydown(evt) {
     return evt.key === 'Escape' || evt.key === 'Esc';
@@ -24,12 +40,30 @@
 
       askModalCustomerName.focus();
 
+      if (nameStorage && askModalCustomerName) {
+        askModalCustomerName.value = nameStorage;
+      }
+
+      if (phoneStorage && askModalCustomerPhone) {
+        askModalCustomerPhone.value = phoneStorage;
+      }
+
+      if (questionStorage && askModalCustomerQuestion) {
+        askModalCustomerQuestion.value = questionStorage;
+      }
+
       document.addEventListener('keydown', onEscPress);
       overlay.addEventListener('click', closeAskModal);
     }
   }
 
   function closeAskModal() {
+    if (isStorageSupport) {
+      localStorage.setItem('askModalCustomerName', askModalCustomerName.value);
+      localStorage.setItem('askModalCustomerPhone', askModalCustomerPhone.value);
+      localStorage.setItem('askModalCustomerQuestion', askModalCustomerQuestion.value);
+    }
+
     askModal.classList.add('ask-form--hidden');
     overlay.classList.add('overlay--hidden');
 
